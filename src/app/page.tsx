6da775a0f55171b270c3a83abb71998ed2da6e16
5JJ <nedin12@naver.com>
2024-10-26 21:01:15 -0700

    [feat] design the structure - add [lang] folder for localization   - cannot use middleware becasue of 'export' option   - so, separate English and other languages - set next.config.mjs file   - not to use image optimization   - becasuse of 'export' option

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+import data from "@data/en.json";
+
+const Home = () => {
+  const dataByLang = data;
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        {dataByLang.test}
+      </div>
     </main>
   );
-}
+};
+
+export default Home;
