a3cd3babbf4b378751f69af285569669262a4e5c
5JJ <nedin12@naver.com>
2024-11-01 23:25:47 -0700

    [feat] add l10n folder to separate l10n related files from common data

--- src/app/_components/profile/Profile.tsx
+++ src/app/_components/profile/Profile.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import ProfileImage from "@images/profile.jpeg";
 import { defaultLang, SupportedLangs } from "../../types";
-import data from "../../../data";
+import data from "../../../data/l10n";
 
 type ProfileProps = {
   lang?: SupportedLangs;
